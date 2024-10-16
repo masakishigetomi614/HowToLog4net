a74f46fdb4759b4f2d77983ce967aea818fe3dd3
masaki.shigetomi <masaki.shigetomi@gmail.com>
2024-10-16 21:25:09 +0900

    log4netの初期版

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+
+public class Program
+{
+    // ロガーの設定
+    private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+    public static void Main(string[] args)
+    {
+        // ログの初期化
+        log.Info("アプリケーションが開始されました。");
+
+        try
+        {
+            // 例としてエラーログを出力
+            throw new Exception("例外が発生しました！");
+        }
+        catch (Exception ex)
+        {
+            log.Error("エラーが発生しました: ", ex);
+        }
+
+        log.Info("アプリケーションが終了します。");
+    }
+}
